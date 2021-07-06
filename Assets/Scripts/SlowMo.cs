ada4669fa5804bc6fb5a4592cc1146d6d3ce0ccd
SokSpig <sokratisspigos@gmail.com>
2021-07-05 17:21:41 -0700

    SlowMotion/Riders

---
+++ Assets/Scripts/SlowMo.cs
@@ -0,0 +1,34 @@
+
+using UnityEngine;
+
+public class SlowMo : MonoBehaviour
+{
+
+    public float slowdownFactor=0.05f;
+    public float slowdownLength = 2f;
+
+ 
+    void Update()
+    {
+        Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+    }
+
+    public void DoSlowmotion ()
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("SlowMotion1");
+
+        if (other.tag == "Player")
+        {
+            Debug.Log("SlowMotion");
+            DoSlowmotion();
+
+        }
+    }
+}
